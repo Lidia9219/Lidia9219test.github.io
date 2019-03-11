dc2921ba80af7ae80dde1fa1999c9b2bfc13f1cf
Lidia Cozmuta <maximlidia59@gmail.com>
2019-03-11 21:00:08 +0200

    cleanup js intro

--- intro.js
+++ intro.js
@@ -1,20 +1,3 @@
-console.info("My first JS file");
-
-var name = "Lidia";
-var employed = "True";
-
-console.warn("employed");
-console.warn(employed);
-
-console.info("Name:");
-console.info(name);
-
-name = "Maxim";
-
-console.info("Name:");
-console.info(name);
-
-
 function getWelcomeMsg() {
     console.log();
     return "Wellcome to my site";
@@ -57,24 +40,9 @@ console.info("summaryElement", summaryElement);
 var color = getColor();
 summaryElement.style.color = color;
 
-// first example 
-function hideHomePage() {
-    document.getElementById('home-page').style.display = 'none';
 
-}
-function showSkillsPage() {
-    document.getElementById('skills-page').style.display = 'block';
-}
 
-function clickOnSkills() {
-   // hideHomePage();
-   // showSkillsPage();
-   hidePage('home-page');
-   hidePage('languages-page');
-   hidePage('about-page');
-   showPage('skills-page');
-}
-// first example - end 
+
 
 function hidePage(page) {
     document.getElementById(page).style.display = 'none';
